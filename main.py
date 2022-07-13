e3262898a795845e17fb3d60f5bfb1ac3c7454cb
Justas Belevičius <84323002+Justas1988@users.noreply.github.com>
2022-07-13 19:58:20 +0300

    Create main.py
    Initial commit.

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+QR Code Generator
+
+Work in progress
+'''
+
+import pyqrcode 
+from pyqrcode import QRCode 
+  
+
+s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+  
+ 
+url = pyqrcode.create(s) 
+  
+
+url.svg("myyoutube.svg", scale = 8)
